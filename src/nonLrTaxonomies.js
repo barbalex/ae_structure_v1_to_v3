9a494b4853aba25ac14275c1d8b180d9de7a6e20
Alexander Gabriel <alex.barbalex@gmail.com>
2016-03-11 17:41:22 +0100

    build nonLrTaxonomies

---
+++ src/nonLrTaxonomies.js
@@ -0,0 +1,32 @@
+'use strict'
+
+module.exports = [
+  {
+    Name: 'CSCF (2009)',
+    Beschreibung: 'Index der Info Fauna. Eigenschaften von 21542 Tierarten',
+    Datenstand: '2009',
+    Link: 'http://www.cscf.ch',
+    Gruppe: 'Fauna'
+  },
+  {
+    Name: 'SISF Index 2 (2005)',
+    Beschreibung: 'Synonymie-Index der Schweizer Flora. Zweite Auflage. Eigenschaften von 7973 Pflanzenarten. Arten mit NR > 1000000 von der FNS provisorisch ergänzt',
+    Datenstand: '2007.05.08',
+    Link: 'http://www.crsf.ch/documents/download/x/ISFS2_20051115.pdf',
+    Gruppe: 'Flora'
+  },
+  {
+    Name: 'Swissfunghi (2011)',
+    Beschreibung: 'WSL, Swissfunghi, Beatrice Senn (Datenzentrum Moose Schweiz)',
+    Datenstand: '2011.06',
+    Link: 'http://www.wsl.ch/dienstleistungen/inventare/pilze_flechten/swissfungi/index_DE',
+    Gruppe: 'Pilze'
+  },
+  {
+    Name: 'NISM (2010)',
+    Beschreibung: 'WSL, Swissfunghi, Beatrice Senn (Datenzentrum Moose Schweiz)',
+    Datenstand: '2010',
+    Link: 'http://www.nism.uzh.ch',
+    Gruppe: 'Moose'
+  }
+]
