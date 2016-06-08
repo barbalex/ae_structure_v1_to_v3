31976af9009a8ce35aaeb162014b071779d4ffe1
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-08 10:11:05 +0200

    optimize code

--- src/buildTaxObjectsFloraLevel1.js
+++ src/buildTaxObjectsFloraLevel1.js
@@ -3,23 +3,21 @@
 const _ = require('lodash')
 const uuid = require('node-uuid')
 
-module.exports = function (db, taxFlora) {
-  return new Promise((resolve, reject) => {
+module.exports = (db, taxFlora) =>
+  new Promise((resolve, reject) => {
     db.view('artendb/prov_baumFlora', {
       group_level: 1
     }, (error, result) => {
       if (error) reject(`error querying view baumFlora: ${error}`)
       const names = _.map(result, (row) => row.key[0])
-      let taxObjectsFloraLevel1 = names.map((name) => {
-        return {
-          _id: uuid.v4(),
-          Typ: 'Taxonomie-Objekt',
-          Taxonomie: taxFlora._id,
-          Name: name
-        }
-      })
-      db.save(taxObjectsFloraLevel1, (error, results) => {
-        if (error) reject(`error saving taxObjectsFloraLevel1 ${error}`)
+      const taxObjectsFloraLevel1 = names.map((name) => ({
+        _id: uuid.v4(),
+        Typ: 'Taxonomie-Objekt',
+        Taxonomie: taxFlora._id,
+        Name: name
+      }))
+      db.save(taxObjectsFloraLevel1, (err, results) => {
+        if (err) reject(`error saving taxObjectsFloraLevel1 ${err}`)
         // update taxObjectsFloraLevel1
         results.forEach((res, i) => {
           taxObjectsFloraLevel1[i]._rev = res.rev
@@ -28,4 +26,3 @@ module.exports = function (db, taxFlora) {
       })
     })
   })
-}
--- src/buildTaxObjectsFloraLevel2.js
+++ src/buildTaxObjectsFloraLevel2.js
@@ -3,14 +3,14 @@
 const _ = require('lodash')
 const uuid = require('node-uuid')
 
-module.exports = function (db, taxFlora, taxObjectsFloraLevel1) {
-  return new Promise((resolve, reject) => {
+module.exports = (db, taxFlora, taxObjectsFloraLevel1) =>
+  new Promise((resolve, reject) => {
     db.view('artendb/prov_baumFlora', {
       group_level: 2
     }, (error, result) => {
       if (error) reject(`error querying view baumFlora: ${error}`)
       const keys = _.map(result, (row) => row.key)
-      let taxObjectsFloraLevel2 = _.map(keys, (key) => {
+      const taxObjectsFloraLevel2 = _.map(keys, (key) => {
         const taxonomie = taxFlora._id
         const familieName = key[0]
         const familieObject = taxObjectsFloraLevel1.find((taxObj) => taxObj.Name === familieName)
@@ -21,11 +21,11 @@ module.exports = function (db, taxFlora, taxObjectsFloraLevel1) {
           Typ: 'Taxonomie-Objekt',
           Taxonomie: taxonomie,
           Name: name,
-          parent: parent
+          parent
         }
       })
-      db.save(taxObjectsFloraLevel2, (error, results) => {
-        if (error) reject(`error saving taxObjectsFloraLevel2 ${error}`)
+      db.save(taxObjectsFloraLevel2, (err, results) => {
+        if (err) reject(`error saving taxObjectsFloraLevel2 ${err}`)
         // update taxObjectsFloraLevel2
         results.forEach((res, i) => {
           taxObjectsFloraLevel2[i]._rev = res.rev
@@ -34,4 +34,3 @@ module.exports = function (db, taxFlora, taxObjectsFloraLevel1) {
       })
     })
   })
-}
