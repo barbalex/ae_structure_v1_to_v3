a841f88b1038649a5fea50d9332e95b61c6d4c7f
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-08 10:10:57 +0200

    optimize code

--- src/buildTaxObjectsMooseLevel1.js
+++ src/buildTaxObjectsMooseLevel1.js
@@ -3,23 +3,21 @@
 const _ = require('lodash')
 const uuid = require('node-uuid')
 
-module.exports = function (db, taxMoose) {
-  return new Promise((resolve, reject) => {
+module.exports = (db, taxMoose) =>
+  new Promise((resolve, reject) => {
     db.view('artendb/prov_baumMoose', {
       group_level: 1
     }, (error, result) => {
       if (error) reject(`error querying view baumMoose: ${error}`)
       const names = _.map(result, (row) => row.key[0])
-      let taxObjectsMooseLevel1 = names.map((name) => {
-        return {
-          _id: uuid.v4(),
-          Typ: 'Taxonomie-Objekt',
-          Taxonomie: taxMoose._id,
-          Name: name
-        }
-      })
-      db.save(taxObjectsMooseLevel1, (error, results) => {
-        if (error) reject(`error saving taxObjectsMooseLevel1 ${error}`)
+      const taxObjectsMooseLevel1 = names.map((name) => ({
+        _id: uuid.v4(),
+        Typ: 'Taxonomie-Objekt',
+        Taxonomie: taxMoose._id,
+        Name: name
+      }))
+      db.save(taxObjectsMooseLevel1, (err, results) => {
+        if (err) reject(`error saving taxObjectsMooseLevel1 ${err}`)
         // update taxObjectsMooseLevel1
         results.forEach((res, i) => {
           taxObjectsMooseLevel1[i]._rev = res.rev
@@ -28,4 +26,3 @@ module.exports = function (db, taxMoose) {
       })
     })
   })
-}
--- src/buildTaxObjectsMooseLevel2.js
+++ src/buildTaxObjectsMooseLevel2.js
@@ -3,17 +3,19 @@
 const _ = require('lodash')
 const uuid = require('node-uuid')
 
-module.exports = function (db, taxMoose, taxObjectsMooseLevel1) {
-  return new Promise((resolve, reject) => {
+module.exports = (db, taxMoose, taxObjectsMooseLevel1) =>
+  new Promise((resolve, reject) => {
     db.view('artendb/prov_baumMoose', {
       group_level: 2
     }, (error, result) => {
       if (error) reject(`error querying view baumMoose: ${error}`)
       const keys = _.map(result, (row) => row.key)
-      let taxObjectsMooseLevel2 = _.map(keys, (key) => {
+      const taxObjectsMooseLevel2 = _.map(keys, (key) => {
         const taxonomie = taxMoose._id
         const klasseName = key[0]
-        const klasseObject = taxObjectsMooseLevel1.find((taxObj) => taxObj.Name === klasseName)
+        const klasseObject = taxObjectsMooseLevel1.find((taxObj) =>
+          taxObj.Name === klasseName
+        )
         const name = key[1]
         const parent = klasseObject._id
         return {
@@ -21,11 +23,11 @@ module.exports = function (db, taxMoose, taxObjectsMooseLevel1) {
           Typ: 'Taxonomie-Objekt',
           Taxonomie: taxonomie,
           Name: name,
-          parent: parent
+          parent
         }
       })
-      db.save(taxObjectsMooseLevel2, (error, results) => {
-        if (error) reject(`error saving taxObjectsMooseLevel2 ${error}`)
+      db.save(taxObjectsMooseLevel2, (err, results) => {
+        if (err) reject(`error saving taxObjectsMooseLevel2 ${err}`)
         // update taxObjectsMooseLevel2
         results.forEach((res, i) => {
           taxObjectsMooseLevel2[i]._rev = res.rev
@@ -34,4 +36,3 @@ module.exports = function (db, taxMoose, taxObjectsMooseLevel1) {
       })
     })
   })
-}
