37360406fb5bfc11f2bb0b7cd6a2e65ccbd911d3
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-08 10:10:53 +0200

    optimize code

--- src/buildTaxObjectsMooseLevel3.js
+++ src/buildTaxObjectsMooseLevel3.js
@@ -3,22 +3,22 @@
 const _ = require('lodash')
 const uuid = require('node-uuid')
 
-module.exports = function (db, taxMoose, taxObjectsMooseLevel1, taxObjectsMooseLevel2) {
-  return new Promise((resolve, reject) => {
+module.exports = (db, taxMoose, taxObjectsMooseLevel1, taxObjectsMooseLevel2) =>
+  new Promise((resolve, reject) => {
     db.view('artendb/prov_baumMoose', {
       group_level: 3
     }, (error, result) => {
       if (error) reject(`error querying view baumMoose: ${error}`)
       const keys = _.map(result, (row) => row.key)
-      let taxObjectsMooseLevel3 = _.map(keys, (key) => {
+      const taxObjectsMooseLevel3 = _.map(keys, (key) => {
         const taxonomie = taxMoose._id
         const klasseObjektName = key[0]
-        const klasseObject = taxObjectsMooseLevel1.find(
-          (taxObj) => taxObj.Name === klasseObjektName
+        const klasseObject = taxObjectsMooseLevel1.find((taxObj) =>
+          taxObj.Name === klasseObjektName
         )
         const familieName = key[1]
-        const familieObject = taxObjectsMooseLevel2.find(
-          (taxObj) => taxObj.Name === familieName && taxObj.parent === klasseObject._id
+        const familieObject = taxObjectsMooseLevel2.find((taxObj) =>
+          taxObj.Name === familieName && taxObj.parent === klasseObject._id
         )
         const name = key[2]
         const parent = familieObject._id
@@ -27,11 +27,11 @@ module.exports = function (db, taxMoose, taxObjectsMooseLevel1, taxObjectsMooseL
           Typ: 'Taxonomie-Objekt',
           Taxonomie: taxonomie,
           Name: name,
-          parent: parent
+          parent
         }
       })
-      db.save(taxObjectsMooseLevel3, (error, results) => {
-        if (error) reject(`error saving taxObjectsMooseLevel3 ${error}`)
+      db.save(taxObjectsMooseLevel3, (err, results) => {
+        if (err) reject(`error saving taxObjectsMooseLevel3 ${err}`)
         // update taxObjectsMooseLevel3
         results.forEach((res, i) => {
           taxObjectsMooseLevel3[i]._rev = res.rev
@@ -40,4 +40,3 @@ module.exports = function (db, taxMoose, taxObjectsMooseLevel1, taxObjectsMooseL
       })
     })
   })
-}
--- src/buildTaxObjectsMooseLevel4.js
+++ src/buildTaxObjectsMooseLevel4.js
@@ -3,29 +3,40 @@
 const _ = require('lodash')
 const uuid = require('node-uuid')
 
-module.exports = function (db, taxMoose, taxObjectsMooseLevel1, taxObjectsMooseLevel2, taxObjectsMooseLevel3, objects) {
-  return new Promise((resolve, reject) => {
+module.exports = (
+  db,
+  taxMoose,
+  taxObjectsMooseLevel1,
+  taxObjectsMooseLevel2,
+  taxObjectsMooseLevel3,
+  objects
+) =>
+  new Promise((resolve, reject) => {
     db.view('artendb/prov_baumMoose', {
       group_level: 5
     }, (error, result) => {
       if (error) reject(`error querying view baumMoose: ${error}`)
       const keys = _.map(result, (row) => row.key)
-      let taxObjectsMooseLevel4 = _.map(keys, (key) => {
+      const taxObjectsMooseLevel4 = _.map(keys, (key) => {
         const taxonomie = taxMoose._id
         const klasseObjektName = key[0]
-        const klasseObject = taxObjectsMooseLevel1.find((taxObj) => taxObj.Name === klasseObjektName)
+        const klasseObject = taxObjectsMooseLevel1.find((taxObj) =>
+          taxObj.Name === klasseObjektName
+        )
         const familieObjektName = key[1]
-        const familieObject = taxObjectsMooseLevel2.find(
-          (taxObj) => taxObj.Name === familieObjektName && taxObj.parent === klasseObject._id
+        const familieObject = taxObjectsMooseLevel2.find((taxObj) =>
+          taxObj.Name === familieObjektName && taxObj.parent === klasseObject._id
         )
         const gattungName = key[2]
-        const gattungObject = taxObjectsMooseLevel3.find(
-          (taxObj) => taxObj.Name === gattungName && taxObj.parent === familieObject._id
+        const gattungObject = taxObjectsMooseLevel3.find((taxObj) =>
+          taxObj.Name === gattungName && taxObj.parent === familieObject._id
         )
         const name = key[3]
         const parent = gattungObject._id
         const objId = key[4]
-        const object = objects.find((obj) => obj._id === objId)
+        const object = objects.find((obj) =>
+          obj._id === objId
+        )
         const eigenschaften = object.Taxonomie.Eigenschaften
         return {
           _id: uuid.v4(),
@@ -36,11 +47,11 @@ module.exports = function (db, taxMoose, taxObjectsMooseLevel1, taxObjectsMooseL
             id: objId,
             Eigenschaften: eigenschaften
           },
-          parent: parent
+          parent
         }
       })
-      db.save(taxObjectsMooseLevel4, (error, results) => {
-        if (error) reject(`error saving taxObjectsMooseLevel4 ${error}`)
+      db.save(taxObjectsMooseLevel4, (err, results) => {
+        if (err) reject(`error saving taxObjectsMooseLevel4 ${err}`)
         // update taxObjectsMooseLevel4
         results.forEach((res, i) => {
           taxObjectsMooseLevel4[i]._rev = res.rev
@@ -49,4 +60,3 @@ module.exports = function (db, taxMoose, taxObjectsMooseLevel1, taxObjectsMooseL
       })
     })
   })
-}
