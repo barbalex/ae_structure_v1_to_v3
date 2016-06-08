344f597803203927d8c26969fccdcbc24ce309af
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-08 10:11:14 +0200

    optimize code

--- src/buildTaxObjectsFaunaLevel2.js
+++ src/buildTaxObjectsFaunaLevel2.js
@@ -3,17 +3,19 @@
 const _ = require('lodash')
 const uuid = require('node-uuid')
 
-module.exports = function (db, taxFauna, taxObjectsFaunaLevel1) {
-  return new Promise((resolve, reject) => {
+module.exports = (db, taxFauna, taxObjectsFaunaLevel1) =>
+  new Promise((resolve, reject) => {
     db.view('artendb/prov_baumFauna', {
       group_level: 2
     }, (error, result) => {
       if (error) reject(`error querying view baumFauna: ${error}`)
       const keys = _.map(result, (row) => row.key)
-      let taxObjectsFaunaLevel2 = _.map(keys, (key) => {
+      const taxObjectsFaunaLevel2 = _.map(keys, (key) => {
         const taxonomie = taxFauna._id
         const klasseName = key[0]
-        const klasseObject = taxObjectsFaunaLevel1.find((taxObj) => taxObj.Name === klasseName)
+        const klasseObject = taxObjectsFaunaLevel1.find((taxObj) =>
+          taxObj.Name === klasseName
+        )
         const name = key[1]
         const parent = klasseObject._id
         return {
@@ -21,11 +23,11 @@ module.exports = function (db, taxFauna, taxObjectsFaunaLevel1) {
           Typ: 'Taxonomie-Objekt',
           Taxonomie: taxonomie,
           Name: name,
-          parent: parent
+          parent
         }
       })
-      db.save(taxObjectsFaunaLevel2, (error, results) => {
-        if (error) reject(`error saving taxObjectsFaunaLevel2 ${error}`)
+      db.save(taxObjectsFaunaLevel2, (err, results) => {
+        if (err) reject(`error saving taxObjectsFaunaLevel2 ${err}`)
         // update taxObjectsFaunaLevel2
         results.forEach((res, i) => {
           taxObjectsFaunaLevel2[i]._rev = res.rev
@@ -34,4 +36,3 @@ module.exports = function (db, taxFauna, taxObjectsFaunaLevel1) {
       })
     })
   })
-}
--- src/buildTaxObjectsFaunaLevel3.js
+++ src/buildTaxObjectsFaunaLevel3.js
@@ -3,22 +3,22 @@
 const _ = require('lodash')
 const uuid = require('node-uuid')
 
-module.exports = function (db, taxFauna, taxObjectsFaunaLevel1, taxObjectsFaunaLevel2) {
-  return new Promise((resolve, reject) => {
+module.exports = (db, taxFauna, taxObjectsFaunaLevel1, taxObjectsFaunaLevel2) =>
+  new Promise((resolve, reject) => {
     db.view('artendb/prov_baumFauna', {
       group_level: 3
     }, (error, result) => {
       if (error) reject(`error querying view baumFauna: ${error}`)
       const keys = _.map(result, (row) => row.key)
-      let taxObjectsFaunaLevel3 = _.map(keys, (key) => {
+      const taxObjectsFaunaLevel3 = _.map(keys, (key) => {
         const taxonomie = taxFauna._id
         const klasseObjektName = key[0]
-        const klasseObject = taxObjectsFaunaLevel1.find(
-          (taxObj) => taxObj.Name === klasseObjektName
+        const klasseObject = taxObjectsFaunaLevel1.find((taxObj) =>
+          taxObj.Name === klasseObjektName
         )
         const ordnungName = key[1]
-        const ordnungObject = taxObjectsFaunaLevel2.find(
-          (taxObj) => taxObj.Name === ordnungName && taxObj.parent === klasseObject._id
+        const ordnungObject = taxObjectsFaunaLevel2.find((taxObj) =>
+          taxObj.Name === ordnungName && taxObj.parent === klasseObject._id
         )
         const name = key[2]
         const parent = ordnungObject._id
@@ -27,11 +27,11 @@ module.exports = function (db, taxFauna, taxObjectsFaunaLevel1, taxObjectsFaunaL
           Typ: 'Taxonomie-Objekt',
           Taxonomie: taxonomie,
           Name: name,
-          parent: parent
+          parent
         }
       })
-      db.save(taxObjectsFaunaLevel3, (error, results) => {
-        if (error) reject(`error saving taxObjectsFaunaLevel3 ${error}`)
+      db.save(taxObjectsFaunaLevel3, (err, results) => {
+        if (err) reject(`error saving taxObjectsFaunaLevel3 ${err}`)
         // update taxObjectsFaunaLevel3
         results.forEach((res, i) => {
           taxObjectsFaunaLevel3[i]._rev = res.rev
@@ -40,4 +40,3 @@ module.exports = function (db, taxFauna, taxObjectsFaunaLevel1, taxObjectsFaunaL
       })
     })
   })
-}
